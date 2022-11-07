de875990146622ecc846148d11a2afb0f8c56cd6
dmitrijpislakov <dpislyakov@bk.ru>
2022-11-08 00:26:19 +0200

    Add Authentication routes

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from backend.apis.version1 import route_general_pages
 from backend.apis.version1 import route_jobs
+from backend.apis.version1 import route_login
 from backend.apis.version1 import route_users
 
 
@@ -11,3 +12,4 @@ api_router.include_router(
 )
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
 api_router.include_router(route_jobs.router, prefix="/jobs", tags=["jobs"])
+api_router.include_router(route_login.router, prefix="/login", tags=["login"])
